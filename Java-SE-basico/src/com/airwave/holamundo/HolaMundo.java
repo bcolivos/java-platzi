752572f82a9d7684b5fd17f4c8ea16990af377c2
BColivos <bcolivos@gmail.com>
2018-09-07 00:13:30 -0500

    Add video tipos de datos

--- Java-SE-basico/src/com/airwave/holamundo/HolaMundo.java
+++ Java-SE-basico/src/com/airwave/holamundo/HolaMundo.java
@@ -5,6 +5,12 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo :)");
+		
+		// Enteros
+		byte edad = 17; // Byte --> -128 a 127
+		short year = 2020; // Short --> -32768 a 32767
+		int id_User = 1001; // Int --> 2147483648 a 2147483647
+		long id_Twitter = 138434834738743L; // Long --> -9223372036854775808	9223372036854775807
 	}
 
 }
